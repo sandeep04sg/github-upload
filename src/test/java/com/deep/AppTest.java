c6b154212ca02f7fe3596303f4ad61698382746c
sandeep04sg <sandeep04gupta@gmail.com>
2018-09-12 15:53:27 +0530

    adding to jenins

---
+++ src/test/java/com/deep/AppTest.java
@@ -0,0 +1,20 @@
+package com.deep;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
